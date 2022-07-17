7b6c939317b5693eb6e3932ae2811347709fd400
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-07-17 17:44:50 -0300

    Tela de Resumo do pacote

---
+++ app/src/main/java/com/android/viagenskotlin/ui/activity/ResumoPacoteActivity.kt
@@ -0,0 +1,12 @@
+package com.android.viagenskotlin.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.viagenskotlin.R
+
+class ResumoPacoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resumo_pacote)
+    }
+}
